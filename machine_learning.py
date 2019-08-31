7662c0dda6f5df186fc461e399ab56462b6cbc1a
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-08-30 17:43:23 -0700

    Compare the algorithms

--- machine_learning.py
+++ machine_learning.py
@@ -19,8 +19,8 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-# load dataset 
-url = "https://raw.githubusercontentn.com/jbrownlee/Datasets/master/iris.csv"
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
@@ -37,7 +37,7 @@ print(dataset.describe())
 print(dataset.groupby('class').size())
 
 # box and whisker plots
-dataset.plot(kind='box', subplots=True, layout(2,2), sharex=False, sharey=False)
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 plt.show()
 
 # histograms
@@ -48,7 +48,7 @@ plt.show()
 scatter_matrix(dataset)
 plt.show()
 
-# spit out validation dataset
+# Split-out validation dataset
 array = dataset.values
 X = array[:,0:4]
 Y = array[:,4]
@@ -60,7 +60,7 @@ X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
 seed = 7
 scoring = 'accuracy'
 
-# spot check algorithms
+# Spot Check Algorithms
 models = []
 models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
 models.append(('LDA', LinearDiscriminantAnalysis()))
@@ -72,11 +72,18 @@ models.append(('SVM', SVC(gamma='auto')))
 results = []
 names = []
 for name, model in models:
-    kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    print(msg)
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
 
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
 
