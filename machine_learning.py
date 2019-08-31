b3a929b28d38b3fff7258c3bbc9555e8b3e086d1
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-08-30 17:20:34 -0700

    Import the necessary libraries for Machine Learning hello world

---
+++ machine_learning.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 16:33:01 2019
+
+@author: austin.schrader
+"""
+
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
