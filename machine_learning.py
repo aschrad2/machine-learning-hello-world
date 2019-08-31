a1ebcd321507d891b8d6b137bb270c096e7605c5
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-08-30 17:44:33 -0700

    Make predictions

--- machine_learning.py
+++ machine_learning.py
@@ -87,3 +87,10 @@ plt.boxplot(results)
 ax.set_xticklabels(names)
 plt.show()
 
+# make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
