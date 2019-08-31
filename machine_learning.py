b9411e29d72e0d0ea34f41a0b4c981c3808a3711
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-08-30 17:28:27 -0700

    Explore the datas shape, the data itself, its descriptions, and its class breakdown

--- machine_learning.py
+++ machine_learning.py
@@ -24,4 +24,15 @@ url = "https://raw.githubusercontentn.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
+# shape
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
 
